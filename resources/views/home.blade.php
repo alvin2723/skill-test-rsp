ec6fe34dafd52903aa11920db254dd387e2d2ab6
MiguelGouw <miguel.gouw@gmail.com>
2020-12-19 11:57:59 +0700

    tambah halaman blog

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,48 +2,56 @@
 
 
 @section('content')
-     <div class="wrap">
-         
-         <header class="header" style="background-image:url('{{asset('image/homepage.jpg')}}');">
-            <div class="overlay">
-                
-            </div>
-         </header>
-         !-- Navigation -->
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="{{asset('image/refactorylogo.png')}}" alt="" width="125" height="52">
-                    </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active navbar-right">
-                            <a class="nav-link" href="#">Home</a>
-                        </li>
-                        <li class="nav-item navbar-right">
-                            <a class="nav-link" href="#">Courses</a>
-                        </li>
-                        <li class="nav-item navbar-right">
-                            <a class="nav-link " href="#">Custom Trainings</a>
-                        </li>
-                        <li class="nav-item navbar-right">
-                            <a class="nav-link " href="#">Case Studies</a>
-                        </li>
-                        <li class="nav-item navbar-right">
-                            <a class="nav-link " href="#">Blog</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-         
-     </div>
-  
-    <main class="container">
-       
-    </main>
+<div class="wrap">
 
-@endsection
+    <header class="header" style="background-image:url('{{asset('image/homepage.jpg')}}');">
+        <div class="overlay">
+
+        </div>
+    </header>
+    !-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{asset('image/refactorylogo.png')}}" alt="" width="125" height="52">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active navbar-right">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item navbar-right">
+                        <a class="nav-link" href="#">Courses</a>
+                    </li>
+                    <li class="nav-item navbar-right">
+                        <a class="nav-link " href="#">Custom Trainings</a>
+                    </li>
+                    <li class="nav-item navbar-right">
+                        <a class="nav-link " href="#">Case Studies</a>
+                    </li>
+                    <li class="nav-item navbar-right">
+                        <a class="nav-link " href="/blog">Blog</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</div>
+
+<main class="container">
+    <div class="text-center">
+        <h1>Apa Yang Refactory Dapat Bantu?</h1>
+    </div>
+    <div class="row mx-md-n5 mt-5">
+        <div class="col-6 px-md-5">
+            <div class="p-3 border bg-light">Custom column padding</div>
+        </div>
+        <div class="col-6 px-md-5">
+            <div class="p-3 border bg-light">Custom column padding</div>
+        </div>
+    </div>
+</main> @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,20 +9,29 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
-    crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
-    
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/glide.core.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/glide.theme.min.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
-   
-    @yield('content')
 
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+
+    <script>
+        const config = {
+            type: "carousel",
+            perView: 3
+        };
+        new Glide('.glide', config).mount()
+    </script>
 </body>
-    
+
 </html>
